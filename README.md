35c3642b3197fb7cbef9aaeffaf7bf3da3f17964
AlbertMartorell <albert.martorell@vicensvivesdigital.com>
2020-02-03 19:19:31 +0100

    Neat some files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MeteoMarto
-Architect coders project about forecast for several cities around the world.
+Project about forecast for several cities around the world.
 
-Utilizaré la api open source Open Wheather https://openweathermap.org/api, para optener los datos necesarios para mostrar la previsión del tiempo
+Utilizaré la api open source Open Wheather https://openweathermap.org/api, para obtener los datos necesarios para mostrar la previsión del tiempo
--- app/src/main/java/com/apps/albertmartorell/meteomarto/MeteoMartoApp.kt
+++ app/src/main/java/com/apps/albertmartorell/meteomarto/MeteoMartoApp.kt
@@ -6,12 +6,8 @@ import com.apps.albertmartorell.meteomarto.framework.db.MeteoMartoDatabase
 
 class MeteoMartoApp : Application() {
 
-    // It is marked with "late-init" to crash if it is not initialized before use (and that's exactly what we want)
+    // It is marked with "late-init" to crash if it is not initialized before use
     lateinit var meteoMartoDatabase: MeteoMartoDatabase
-    //    val weatherRepository =
-//        WeatherRepository(ImpWeatherServerSource(), ImpWeatherDeviceSource(this))
-    //lateinit var weatherRepository: WeatherRepository
-    val openWeatherMapApiKey = "5e11044c499339f5ede8d58d85d134d3"
 
     override fun onCreate() {
 
@@ -19,26 +15,7 @@ class MeteoMartoApp : Application() {
 
         meteoMartoDatabase =
             Room.databaseBuilder(this, MeteoMartoDatabase::class.java, "MeteoMartoDatabase").build()
-//        weatherRepository =
-//            WeatherRepository(ImpWeatherServerSource(), ImpWeatherDeviceSource(this))
-
-//        meteoMartoDatabase =
-//            Room.databaseBuilder(this, MeteoMartoDatabase::class.java, "MeteoMartoDatabase").build()
 
     }
 
-    // This injects all the dependencies into the ViewModelFactory. It creates ViewModels in the app and passes interactor dependencies to them
-//    MajesticViewModelFactory.inject(
-//    this,
-//    Interactors(
-//    AddBookmark(bookmarkRepository),
-//    GetBookmarks(bookmarkRepository),
-//    RemoveBookmark(bookmarkRepository),
-//    AddDocument(documentRepository),
-//    GetDocuments(documentRepository),
-//    RemoveDocument(documentRepository),
-//    GetOpenDocument(documentRepository),
-//    SetOpenDocument(documentRepository)
-//    )
-//    )
 }
